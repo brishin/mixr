0ae3044af95286d7ed8b2f07f0a39c2aabbfb66d
Brian Donghee Shin <shindonghee@gmail.com>
2012-07-21 15:25:17 -0400

    Basic fb login

--- api/app.py
+++ api/app.py
@@ -4,10 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+  return app.send_static_file('index.html')
+
+@app.route('/channel.html')
+def channel():
+  return app.send_static_file('channel.html')    
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+  # Bind to PORT if defined, otherwise default to 5000.
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
