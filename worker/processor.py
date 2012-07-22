58c9268c16eac1f064728a176a4e79d2925a9546
Brian Donghee Shin <shindonghee@gmail.com>
2012-07-21 20:33:46 -0400

    Worker typos

--- worker/processor.py
+++ worker/processor.py
@@ -6,5 +6,8 @@ r = redis.StrictRedis(host='taleyarn.com', port=6379, db=0)
 
 def worker_daemon():
   while True:
-    msg = json.loads(redis.blpop('queue', 0))
+    msg = json.loads(r.blpop('queue', 0))
     print msg
+
+if __name__ == '__main__':
+  worker_daemon()
