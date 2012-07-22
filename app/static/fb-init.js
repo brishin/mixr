7318f8915120a4e6cf642f79c2adac293184aa75
Brian Donghee Shin <shindonghee@gmail.com>
2012-07-21 21:09:40 -0400

    More permissions

--- app/static/fb-init.js
+++ app/static/fb-init.js
@@ -31,6 +31,9 @@ window.fbAsyncInit = function() {
         document.getElementById('auth-loggedout').style.display = 'none';
         document.getElementById('auth-loggedin').style.display = 'block';
       });
+      FB.login(function(response) {
+        // handle the response
+      }, {scope: 'user_actions.music,friends_actions.music'});
     } else {
       // user has not auth'd your app, or is not logged into Facebook
       document.getElementById('auth-loggedout').style.display = 'block';
--- worker/processor.py
+++ worker/processor.py
@@ -7,8 +7,8 @@ r = redis.StrictRedis(host='taleyarn.com', port=6379, db=0)
 def worker_daemon():
   while True:
     response = r.blpop('processUser', 0)
-    msg = json.loads(response[1])
-    print msg
+    if response[0] == 'processUser':
+      user_id = json.loads(response[1])
 
 if __name__ == '__main__':
   worker_daemon()
