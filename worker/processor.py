95c3a52c5ff6c80b8d64e29761ff0515c038dbbb
Brian Donghee Shin <shindonghee@gmail.com>
2012-07-21 21:58:29 -0400

    Loading

--- worker/processor.py
+++ worker/processor.py
@@ -8,7 +8,7 @@ def worker_daemon():
   while True:
     response = r.blpop('processUser', 0)
     if response[0] == 'processUser':
-      user_id = json.loads(response[1])
+      user_id = response[1]
 
 if __name__ == '__main__':
   worker_daemon()
