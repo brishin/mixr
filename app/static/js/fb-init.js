9e8eec4640f24498058feff0d8554db5e64102a0
Jane Cakemaster <janecakemaster@gmail.com>
2012-07-22 08:32:00 -0400

    COOL JQUERY THINGS

--- app/static/js/fb-init.js
+++ app/static/js/fb-init.js
@@ -32,6 +32,19 @@ window.fbAsyncInit = function() {
     if (response.authResponse) {
       // user has auth'd your app and is logged into Facebook
       var data = {'authKey': FB.getAccessToken(), 'userID': FB.getUserID()};
+      // put logo movement here
+      var windowHeight = $(window).height();
+
+      $('.bottom').hide();
+      $('.top').animate({
+        padding: '0px',
+        width: '180px'
+      },
+      1000);      
+      $('.logo').animate({
+        margin: '0px'
+      },
+      1000);
       $.post('https://'+window.location.hostname+'/api/login', data, function(){
         document.getElementById('auth-loggedout').style.display = 'none';
         document.getElementById('auth-loggedin').style.display = 'block';
