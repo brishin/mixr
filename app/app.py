d97a3c81891b8a531e80683e0f4b688db944a196
Andrew Pope <drewii2ii@gmail.com>
2012-07-22 02:10:43 -0400

    Modified app.py for test url, and worker for music data

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
-import os
-from flask import Flask, abort, request, make_response, current_app
-import json
+import os, random, math
+from flask import Flask, abort, request, make_response, current_app, json, Response
 from datetime import timedelta
 from functools import update_wrapper
 import redis
@@ -17,6 +16,32 @@ def index():
 def channel():
   return app.send_static_file('channel.html')
 
+@app.route('/api/song', methods=['GET'])
+def song():
+  test = {
+    'title': ["Suggestions Rock", "Distress Is The Root Of All Evil", "Heat Is Painless", "Dying Fears", "Regrets For The Fun Of It", "Ambitions In A Glass", "Signs Just Keep Getting Better", "Can't Stop The Misadventure", "Remembering Adventure", "Friendship Makes Me Sick"],
+    'artist': ["Abramovic, Marina", "Agam, Yaacov", "Alajalov, Constantin", "Angelico, Fra (Giovanni da Fiesole)", "Anon", "Arbus, Diane", "Archipenko, Alexander", "Arcimboldo, Giuseppe", "Arp, Jean (Hans)", "Audubon, John James"],
+    'artwork': ["http://images.grooveshark.com/static/albums/50_4685728.jpg", "http://images.grooveshark.com/static/albums/50_2393332.jpg", "http://images.grooveshark.com/static/albums/50_126423.jpg", "http://images.grooveshark.com/static/albums/50_4274427.jpg", "http://images.grooveshark.com/static/albums/50_3929125.jpg", "http://images.grooveshark.com/static/albums/50_6722700.jpg", "http://images.grooveshark.com/static/albums/50_8066301.jpg", "http://images.grooveshark.com/static/albums/50_1270594.jpg", "http://images.grooveshark.com/static/albums/50_146568.jpg", "http://images.grooveshark.com/static/albums/50_5399658.jpg"],
+    'album': ["He's The DJ, I'm The Rapper", "Bigger and Deffer", "The Black Album", "Fantastic Vol. Two", "Blowout Comb", "Greatest Misses", "No One Can Do It Better", "Back For The First Time", "Things Fall Apart", "Dr. Octagon"],
+    'length': ["303.000000", "263.000000", "353.000000", "245.000000", "185.000000", "234.000000", "986.000000", "234.000000", "125.000000", "923.000000"],
+    'location': ["test", "test", "test", "test", "test", "test", "test", "test", "test", "test"]
+  }
+  random.seed()
+  data = {
+        'title': test['title'][int(math.floor(random.random()*10))],
+        'artist': test['artist'][int(math.floor(random.random()*10))],
+        'artwork': test['artwork'][int(math.floor(random.random()*10))],
+        'album': test['album'][int(math.floor(random.random()*10))],
+        'length': test['length'][int(math.floor(random.random()*10))],
+        'location': test['location'][int(math.floor(random.random()*10))]
+    }
+  js = json.dumps(data)
+
+  resp = Response(js, status=200, mimetype='application/json')
+  resp.headers['Link'] = 'http://mixr.herokuapp.com'
+
+  return resp
+
 @app.route('/api/login', methods=['POST'])
 def login():
   app.logger.debug(str(request.form))
@@ -25,9 +50,11 @@ def login():
     authKey = request.form['authKey']
     r.lpush('users', userID)
     r.hset(userID, 'authKey', authKey)
-    graph = facebook.GraphAPI(authKey)
-    pic = graph.get_object('me/picture', type='square')
-    r.hset(userID, 'pic', pic.get('url', None))
+    # Uneeded, we can just insert the userID into the url
+    # on the pag
+    # graph = facebook.GraphAPI(authKey)
+    # pic = graph.get_object('me/picture', type='square')
+    # r.hset(userID, 'pic', pic.get('url', None))
     r.rpush('processUser', userID)
     return 'success'
   abort(400)
--- worker/processor.py
+++ worker/processor.py
@@ -9,6 +9,9 @@ def worker_daemon():
     response = r.blpop('processUser', 0)
     if response[0] == 'processUser':
       user_id = response[1]
+      oauth = r.get(user_id).get('authKey')
+      graph = facebook.GraphAPI(oauth)
+      songs = facebook.get_connections("me", "music.listens")
 
 if __name__ == '__main__':
   worker_daemon()
